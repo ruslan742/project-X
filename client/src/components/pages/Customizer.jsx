e8843839310e20afb2b3510205ad4170140aac1d
1replica5 <antonvolsky@gmail.com>
2024-07-15 13:49:22 +0300

    R6

--- client/src/components/pages/Customizer.jsx
+++ client/src/components/pages/Customizer.jsx
@@ -4,7 +4,12 @@ import { useSnapshot } from "valtio";
 import { slideAnimation } from "../../config/motion";
 import { reader } from "../../config/helpers";
 import { ColorPicker, CustomButton, FilePicker, Tab } from "..";
-import { ClothTabs, DecalTypes, EditorTabs, FilterTabs } from "../../config/constants";
+import {
+  ClothTabs,
+  DecalTypes,
+  EditorTabs,
+  FilterTabs,
+} from "../../config/constants";
 import state from "../../store";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
@@ -14,10 +19,8 @@ import Sock from "../../canvas/Sock";
 import CameraRig from "../../canvas/CameraRig";
 import LogoPicker from "../models/LogoPicker";
 import TexturePicker from "../models/TexturePicker";
-// import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
-// import { Loader } from "../../HOC/Loader";
-import { toast } from 'react-hot-toast';
-import axios from 'axios';
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 function Customizer() {
   const tabRef = useRef(null);
@@ -33,7 +36,9 @@ function Customizer() {
   });
   useEffect(() => {
     state.price =
-      ((snap.cloth === "sock" ? 100 : snap.cloth === "shirt" ? 500 : 1000) + (snap.isFullTexture ? 300 : 0) + (snap.isLogoTexture ? 200 : 0)) *
+      ((snap.cloth === "sock" ? 100 : snap.cloth === "shirt" ? 500 : 1000) +
+        (snap.isFullTexture ? 300 : 0) +
+        (snap.isLogoTexture ? 200 : 0)) *
       snap.qty;
   }, [snap.cloth, snap.isFullTexture, snap.isLogoTexture, snap.qty]);
 
@@ -44,7 +49,14 @@ function Customizer() {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker prompt={prompt} setPrompt={setPrompt} generatingImg={generatingImg} handleSubmit={handleSubmit} />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
     }
@@ -161,7 +173,6 @@ function Customizer() {
     });
 
     state.totalPrice = snap.totalPrice + snap.price;
-    //setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
     if (checkProductInCart) {
       const updatedCartItems = snap.cartItems.map((cartProduct) => {
@@ -171,23 +182,22 @@ function Customizer() {
             quantity: cartProduct.quantity + quantity,
           };
       });
-      state.cartItems=updatedCartItems
-      console.log('cartitems',snap.cartItems)
-      //setCartItems(updatedCartItems);
+      state.cartItems = updatedCartItems;
+      console.log("cartitems", snap.cartItems);
     } else {
-      state.cartItems=[...snap.cartItems,product]
-      console.log('cartitems',snap.cartItems)
-      // product.quantity = quantity;
-
-      // setCartItems([...cartItems, { ...product }]);
+      state.cartItems = [...snap.cartItems, product];
+      console.log("cartitems", snap.cartItems);
     }
     console.log(snap.cartItems);
     toast.success(`Item added to the cart.`);
     try {
-      await axios.post('api/bascet', {usermail:snap.email,cloth:JSON.stringify(product)});
-   } catch (error) {
-     alert(error.response.data.message || 'Oops!');
-   }
+      await axios.post("api/bascet", {
+        usermail: snap.email,
+        cloth: JSON.stringify(product),
+      });
+    } catch (error) {
+      alert(error.response.data.message || "Oops!");
+    }
   };
 
   return (
@@ -209,22 +219,39 @@ function Customizer() {
       <AnimatePresence>
         (
         <>
-          <motion.div key="custom" className="absolute top-0 left-0 z-10" {...slideAnimation("left")}>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container ">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => setActiveEditorTab(tab.name)} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
                 {generateTabContent()}
               </div>
             </div>
           </motion.div>
-          <motion.div key="buttons" className="flex flex-col items-center absolute top-0 right-10 z-10" {...slideAnimation("right")}>
+          <motion.div
+            key="buttons"
+            className="flex flex-col items-center absolute top-0 right-10 z-10"
+            {...slideAnimation("right")}
+          >
             <div className="flex items-center min-h-screen">
               <div className="buttontabs-container ">
-                <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">${snap.price}</div>
+                <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  ${snap.price}
+                </div>
                 <form className="max-w-xs mx-auto">
-                  <label for="quantity-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  <label
+                    for="quantity-input"
+                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                  >
                     Choose quantity:
                   </label>
                   <div className="relative flex items-center max-w-[8rem]">
@@ -242,7 +269,13 @@ function Customizer() {
                         fill="none"
                         viewBox="0 0 18 2"
                       >
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M1 1h16"
+                        />
                       </svg>
                     </button>
                     <input
@@ -269,7 +302,13 @@ function Customizer() {
                         fill="none"
                         viewBox="0 0 18 18"
                       >
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 1v16M1 9h16"
+                        />
                       </svg>
                     </button>
                   </div>
@@ -278,15 +317,34 @@ function Customizer() {
                   type="filled"
                   title="Добавить в корзину"
                   handleClick={() =>
-                    onAdd({ cloth: snap.cloth, color: snap.color, logo: (snap.isLogoTexture?snap.logoDecal:null), texture:( snap.isFullTexture?snap.fullDecal:null), quantity: snap.qty,price:snap.price }, snap.qty)
+                    onAdd(
+                      {
+                        cloth: snap.cloth,
+                        color: snap.color,
+                        logo: snap.isLogoTexture ? snap.logoDecal : null,
+                        texture: snap.isFullTexture ? snap.fullDecal : null,
+                        quantity: snap.qty,
+                        price: snap.price,
+                      },
+                      snap.qty
+                    )
                   }
-                  customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                  customStyles="w-full px-4 py-2.5 font-bold text-sm"
                 />
-                <CustomButton type="filled" title="Добавить в избранное" handleClick={() => {}} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
+                <CustomButton
+                  type="filled"
+                  title="Добавить в избранное"
+                  handleClick={() => {}}
+                  customStyles="w-full px-4 py-2.5 font-bold text-sm"
+                />
               </div>
             </div>
           </motion.div>
-          <motion.div ref={tabRef} className="filtertabs-container" {...slideAnimation("up")}>
+          <motion.div
+            ref={tabRef}
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
             {FilterTabs.map((tab) => (
               <Tab
                 key={tab.name}
@@ -303,7 +361,13 @@ function Customizer() {
           </motion.div>
           <motion.div className="clothtabs-container" {...slideAnimation("up")}>
             {ClothTabs.map((tab) => (
-              <Tab key={tab.name} tab={tab} isClothTab isActiveTab={tab.name === snap.cloth} handleClick={() => handleActiveClothTab(tab.name)} />
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isClothTab
+                isActiveTab={tab.name === snap.cloth}
+                handleClick={() => handleActiveClothTab(tab.name)}
+              />
             ))}
           </motion.div>
         </>
